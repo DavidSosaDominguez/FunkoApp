6aee3ba3a0eaefe5313c453d7c4eeeddf8bf1044
RReDDpy <grandeandre23@gmail.com>
2025-03-14 15:02:56 +0000

    JavaScript fetch tests 2

--- code/scripts/Imagefetch.js
+++ code/scripts/Imagefetch.js
@@ -1,9 +1,21 @@
-let Imagefetch = fetch("../../resource-images/images.json")
-                    .then(response => {
-                        if(!response.ok){
-                            throw new Error("Could not find the resource");
-                        }
-                        return response.json();
-                    })
-                    .then(data => {console.log(data)})
-                    .catch(error => console.log(error));
+async function Imagefetch() {
+    try{
+
+        const response = await fetch("../../resource-images/images.json");
+
+       if (!response.ok){
+           throw new Error("Could not fetch resource");
+       }
+
+       const data = await response.json();
+       const img = data.imagen1.url;
+
+       const imgElement = document.getElementById("product_image");
+       if (!imgElement){throw new Error("wtf is this im boutta crash out")}
+       imgElement.src = img;
+       console.log(data);
+    }
+    catch(error){
+        console.error(error);
+    }
+}
