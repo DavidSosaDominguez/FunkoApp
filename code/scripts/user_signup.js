3245f9d626efb64f374b2ccb15bcffa176701305
Miguel <mcasher113@gmail.com>
2025-03-25 17:50:25 +0000

    Changed route of user sign up

--- code/scripts/user_signup.js
+++ code/scripts/user_signup.js
@@ -36,7 +36,7 @@ function registerUser(event) {
 
     sessionStorage.setItem('user', stringUser);
 
-    window.open('../pages/new_index.html', '_self');
+    window.open('../pages/index.html', '_self');
 }
 
 
